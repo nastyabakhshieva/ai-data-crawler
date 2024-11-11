aa066dd1781df42f4ef52a55bb99c9a43d572d3e
anastasiya_bakhshyieva <anastasiya.bakhshieva@outlook.com>
2024-11-11 17:09:50 +0400

    Spring Initializr
    ___________________________
    
    What: Added boilerplate spring configuration from Initializer

---
+++ src/main/java/com/petproject/ai_data_crawler/AiDataCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.ai_data_crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDataCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDataCrawlerApplication.class, args);
+	}
+
+}
